ec3cddd3a896f08b05d8d09a22646577b5f13e00
heyruchika-01 <ruchikasingh593@gmail.com>
2025-12-25 23:04:54 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,7 +5,7 @@
 // import { useContext } from 'react';
 // import { AdminContext } from './context/AdminContext';
 // import Navbar from './components/Navbar';
-// import Sidebar from './components/sidebar';
+// import Sidebar from './components/Sidebar';
 // import { Route, Routes } from 'react-router-dom';
 // import Dashboard from './pages/Admin/Dashboard';
 // import AllAppointments from './pages/Admin/AllAppointments';
